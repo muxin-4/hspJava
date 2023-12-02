7bd31052d080c201221069a65f9023808cbeb755
王冠宇 <wangguanyu@wangguanyudeMacBook-Pro.local>
2023-12-02 17:51:19 +0800

    feat: 第三课 注释

---
+++ course3/Comment03.java
@@ -0,0 +1,22 @@
+/**
+ * @author 王刚
+ * @version 1.0
+ */
+public class Comment03 {
+
+    // 编写一个main方法
+    public static void main(String[] args) {
+
+        /**
+         * 示意 =>  可读性很好
+         * 下面代码完成 两个数相加
+         * 定义变量
+         */
+        int n1 = 10;
+        int n2 = 30;
+
+        int sum = n1 + n2;
+
+        System.out.println("结果=" + sum);
+    }
+}
