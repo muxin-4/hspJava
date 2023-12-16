8a592ae28db46f6df4e08419189bd54265fc9c2a
王冠宇 <wgy952046097@gmail.com>
2023-12-16 14:42:20 +0800

    feat: 整型

---
+++ chapter2/course3/IntDetail.java
@@ -0,0 +1,8 @@
+public class IntDetail {
+    public static void main(String[] args) {
+        // Java的整型常量（具体值）默认为int型，声明long型常量须在后面加‘l’或’L‘
+        int n1 = 1; // 4个字符
+        // int n2 = 1L; // 错误: 不兼容的类型: 从long转换到int可能会有损失
+        long n3 = 1L;
+    }
+}
