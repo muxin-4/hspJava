2ef33762b03decaf040fb6a9f36c912dc72ee228
muxin <wgy952046097@gmail.com>
2024-12-05 18:56:03 +0800

    feat: 0086 四种进制介绍1

---
+++ chapter4/BinaryTest.java
@@ -0,0 +1,22 @@
+public class BinaryTest {
+    // 编写一个main方法
+    public static void main(String[] args) {
+        // 二进制
+        int n1 = 0b1010;
+        // 十进制
+        int n2 = 1010;
+        // 八进制
+        int n3 = 01010;
+        // 十六进制
+        int n4 = 0x10101;
+
+        System.out.println("n1=" + n1); // n1=10
+        System.out.println("n2=" + n2); // n2=1010
+        System.out.println("n3=" + n3); // n3=520
+        System.out.println("n4=" + n4); // n4=65793
+
+
+
+
+    }
+}
