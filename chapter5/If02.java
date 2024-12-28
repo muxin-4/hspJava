2c821211bdb4f765784e979666b91ee7296540db
muxin-4 <952046097@qq.com>
2024-12-28 13:45:42 +0800

    feat: 0106 双分支使用

--- chapter5/If02.java
+++ chapter5/If02.java
@@ -6,21 +6,23 @@ public class If01 {
     public static void main(String[] args) {
         /**
          * 编写一个程序，可以输入人的年龄
-         * 如果该同志的年龄大于18岁，则输出"你年龄大于18，要对自己的行为负责，送入监狱"
+         * 如果该同志的年龄不大于18岁，则输出"你年龄大于18，要对自己的行为负责，送入监狱"。否则，输出"你的年龄不大这次放过你了"
          *
          * 技术设计
          * 1.接收输入的年龄，应该定义一个Scanner对象
          * 2.把年龄保存到一个变量 int age
-         * 3.使用if判断，输出对应信息
+         * 3.使用if-else判断，输出对应信息
          */
         // 1.接收输入的年龄，应该定义一个Scanner对象
         Scanner myScanner = new Scanner(System.in);
         System.out.println("请输入年龄");
         // 2.把年龄保存到一个变量 int age
         int age = myScanner.nextInt();
-        // 3.使用if判断，输出对应信息
+        // 3.使用if-else判断，输出对应信息
         if (age > 18) {
             System.out.println("你年龄大于18，要对自己的行为负责，送入监狱");
+        }else {
+            System.out.println("你的年龄不大这次放过你了");
         }
         System.out.println("程序继续...");
     }
