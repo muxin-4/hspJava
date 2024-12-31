d64a8c14d17f9c1f5df187cef3d28f84ac648411
muxin <wgy952046097@gmail.com>
2024-12-31 13:18:47 +0800

    feat: 0099 位运算详解2

---
+++ chapter4/BitOperator2.java
@@ -0,0 +1,8 @@
+public class BitOperator2 {
+    public static void main(String[] args) {
+        System.out.println(1 >> 2); // 1 / 2^2 = 0
+        System.out.println(1 << 2); // 1 * 2^2 = 4
+        System.out.println(4 << 3); // 4 * 2^3 = 32
+        System.out.println(15 >> 2); // 15 / 2^2 = 3
+    }
+}
