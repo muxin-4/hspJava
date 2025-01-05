b4056fa75d015b7217bd9de6f57828fe11107bf7
muxin <wgy952046097@gmail.com>
2025-01-05 16:31:11 +0800

    feat: ### 0156 数组快速入门

--- chapter6/Array01.java
+++ chapter6/Array01.java
@@ -17,9 +17,33 @@ public class Array01 {
         double hen5 = 2;
         double hen6 = 50;
 
-        double totalWeight = hen1 + hen2 + hen3 + hen4 + hen5 + hen6;
-        System.out.println("The total weight is " + totalWeight);
-        double avgWeight = totalWeight / 6;
-        System.out.println("The average weight is: " + avgWeight);
+        System.out.println("使用传统方式解决");
+        double totalWeight1 = hen1 + hen2 + hen3 + hen4 + hen5 + hen6;
+        System.out.println("The total weight is " + totalWeight1);
+        double avgWeight1 = totalWeight1 / 6;
+        System.out.println("The average weight is: " + avgWeight1);
+
+        // 比如我们用数组来解决上一个问题。
+        // 定义一个数组
+        // 老韩解读
+        // 1.double[] 表示是double类型的数组，数组名hens
+        // 2.{3, 5, 1, 3.4, 2, 50} 表示数组的值/元素
+        double[] hens = {3, 5, 1, 3.4, 2, 50};
+
+        // 遍历数组得到数组的所有元素的和，使用for循环
+        // 老韩解读
+        // 1.我们可以通过hens[下标]来访问数组的元素
+        // 下标是从 0 开始编号的，比如第1个元素就是 hens[0]，第2个元素hens[1]，依次类推
+        // 2.通过for就可以循环的访问数组的值/元素
+        // 3.使用一个变量totalWeight将各个元素累积
+        System.out.println("使用数组解决");
+        double totalWeight2 = 0;
+        for (int i = 0; i < hens.length; i++) {
+            System.out.println("第" + (i + 1) + "个元素的值=" + hens[i]);
+            totalWeight2 += hens[i];
+        }
+
+        System.out.println("The total weight is " + totalWeight2);
+        System.out.println("The average weight is: " + (totalWeight2 / hens.length));
     }
 }
